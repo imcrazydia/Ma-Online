a34d5f583f48b2b67e2873428fc485a345c03fb2
Diaq <imcrazydia@gmail.com>
2021-04-20 21:51:30 +0200

    added upload page view

--- ma_online/resources/views/navigation-menu.blade.php
+++ ma_online/resources/views/navigation-menu.blade.php
@@ -100,6 +100,10 @@
                                 {{ __('Profile') }}
                             </x-jet-dropdown-link>
 
+                            <x-jet-dropdown-link href="{{ route('upload') }}">
+                                {{ __('Upload') }}
+                            </x-jet-dropdown-link>
+
                             @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
                                 <x-jet-dropdown-link href="{{ route('api-tokens.index') }}">
                                     {{ __('API Tokens') }}
