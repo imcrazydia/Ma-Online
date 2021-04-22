5a50ccc65b7baf2e161e57574709e727ed71abfd
Dani Rumping <43114717+PrinceCosmic@users.noreply.github.com>
2021-04-22 13:38:22 +0200

    Login pagina style update (WIP)
    In deze commit heb ik de styling van de authentication forms aangepast, is nog een wip. hiernaast is er ook een override gemaakt van de jetstream components. Onder resources\views\vendor\jetstream\components

--- ma_online/resources/views/auth/login.blade.php
+++ ma_online/resources/views/auth/login.blade.php
@@ -1,16 +1,20 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+        <link rel="stylesheet" href="{{ asset('css/style.css')}}">
+            <!-- <x-jet-authentication-card-logo /> -->
+
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
 
         @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
+        <div class="mb-4 font-medium text-sm text-green-600">
+            {{ session('status') }}
+        </div>
         @endif
+
+
 
         <form method="POST" action="{{ route('login') }}">
             @csrf
@@ -21,22 +25,22 @@
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-label for="password" value="{{ __('Wachtwoord') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-white">{{ __('Onthoud mij') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
+                <a class="underline text-sm text-white hover:text-gray-900" href="{{ route('password.request') }}">
+                    {{ __('Wachtwoord vergeten?') }}
+                </a>
                 @endif
 
                 <x-jet-button class="ml-4">
--- ma_online/resources/views/navigation-menu.blade.php
+++ ma_online/resources/views/navigation-menu.blade.php
@@ -6,14 +6,14 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('home') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                        
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Profiel') }}
                     </x-jet-nav-link>
                 </div>
             </div>
