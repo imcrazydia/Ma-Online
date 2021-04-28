9aabfdaad7dcce38214c6dfe7c9d5122e46d0fe4
Dani Rumping <43114717+PrinceCosmic@users.noreply.github.com>
2021-04-28 14:25:08 +0200

    Update login ready
    Dit zou in feite de laatste revisie van de login page moeten zijn zonder feedback. Na feedback zou er nog eventuele aanpassingen komen.
    
    - Responsive werkt nu
    - Inputfields leesbaar
    - achtergrond toegevoegd

--- ma_online/resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ ma_online/resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,9 +1,9 @@
-<div class="background-image-login min-h-screen flex flex-col pt-6 sm:pt-0 bg-gray-100">
+<div class="background-image-login min-h-screen flex flex-col sm:pt-0 bg-gray-100">
     <div>
         {{ $logo }}
     </div>
 
-    <div class="w-2/5 p-44 h-screen align-middle bg-black shadow-md overflow-hidden text-white-600">
+    <div class="authentication-width lg:w-2/5 md:w-3/5 lg:p-44 md:p-8 h-screen align-middle bg-black shadow-md overflow-hidden text-white-600">
         {{ $slot }}
     </div>
 </div>
