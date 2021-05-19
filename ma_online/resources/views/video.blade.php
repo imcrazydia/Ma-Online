57eb1027fd78c280d990714d490fb275305059af
Dani Rumping <43114717+PrinceCosmic@users.noreply.github.com>
2021-05-19 23:17:48 +0200

    Update video.blade.php

--- ma_online/resources/views/video.blade.php
+++ ma_online/resources/views/video.blade.php
@@ -7,19 +7,24 @@
         </x-slot>
 
         <div class="py-6">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <div class="max-w-7xl mx-auto h-screen py-6 px-4 sm:px-6 lg:px-8 bg-ma-light-gray">
                 <div class="youtube-video-container">
                     <iframe
-                      width="560"
-                      height="315"
-                      src="https://www.youtube.com/embed/{{ $video->video_id }}?autoplay=1"
-                      frameborder="0"
-                      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                      allowfullscreen
+                        width="560"
+                        height="315"
+                        src="https://www.youtube.com/embed/{{ $video->video_id }}?autoplay=1"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen
                     ></iframe>
                 </div>
                 <div class="py-6">
-                    <p class="text-white">{{ $video->description }}</p>
+                    <div class="video-description">
+                        <h2 class="text-ma-white font-bold text-2xl pb-3">
+                            {{ __($video->title) }}
+                        </h2>
+                        <p class="text-white text-sm pt-2 border-t-2">{{ $video->description }}</p>
+                    </div>
                 </div>
             </div>
         </div>
