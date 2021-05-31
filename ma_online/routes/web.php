f46aad6ccbb5593b80e9a607ec2a5bc700a908c1
Diaq <imcrazydia@gmail.com>
2021-05-31 17:57:18 +0200

    Added admin middleware

--- ma_online/routes/web.php
+++ ma_online/routes/web.php
@@ -18,20 +18,27 @@ use App\Http\Controllers\VideoController;
 
 Route::get('/', [UploadVideoController::class, 'index'])->name('home');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/profiel/{user}', [ProfileController::class, 'index'])->name('profiel');
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/upload', function () {
-    return view('upload');
-})->name('upload');
-Route::middleware(['auth:sanctum', 'verified'])->post('/upload/create', [UploadVideoController::class, 'youtubeCreate'])->name('create');
-Route::middleware(['auth:sanctum', 'verified'])->post('/store', [UploadVideoController::class, 'store'])->name('store');
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/video/{id}', [VideoController::class, 'index'])->name('video');
-Route::middleware(['auth:sanctum', 'verified'])->get('/video/{id}/edit/{user}', [VideoController::class, 'edit'])->name('edit');
-Route::middleware(['auth:sanctum', 'verified'])->post('/update', [VideoController::class, 'update'])->name('update');
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/video/{id}/delete/{user}', [VideoController::class, 'delete'])->name('delete');
-Route::middleware(['auth:sanctum', 'verified'])->get('/video/{id}/destroy/{user}', [VideoController::class, 'destroy'])->name('destroy');
-
 Route::get('/search/', [VideoController::class, 'search'])->name('search');
 Route::get('/tagSearch/', [VideoController::class, 'tagSearch'])->name('tagSearch');
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::get('/profiel/{user}', [ProfileController::class, 'index'])->name('profiel');
+
+    Route::get('/upload', function () {
+        return view('upload');
+    })->name('upload');
+    Route::post('/upload/create', [UploadVideoController::class, 'youtubeCreate'])->name('create');
+    Route::post('/store', [UploadVideoController::class, 'store'])->name('store');
+
+    Route::get('/video/{id}', [VideoController::class, 'index'])->name('video');
+    Route::get('/video/{id}/edit/{user}', [VideoController::class, 'edit'])->name('edit');
+    Route::post('/update', [VideoController::class, 'update'])->name('update');
+
+    Route::get('/video/{id}/delete/{user}', [VideoController::class, 'delete'])->name('delete');
+    Route::get('/video/{id}/destroy/{user}', [VideoController::class, 'destroy'])->name('destroy');
+});
+
+Route::group(['prefix'     => 'admin',
+              'middelware' => 'auth:sanctum', 'verified', 'admin'], function() {
+    Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
+});
