74b6827d0a43fd274d1b1eef8f129a7f74bb6588
Diaq <imcrazydia@gmail.com>
2021-06-01 17:21:48 +0200

    removed duplication of home page videos

--- ma_online/resources/views/welcome.blade.php
+++ ma_online/resources/views/welcome.blade.php
@@ -452,34 +452,37 @@
 
 
     {{-- NIET INGELOGD --}}
-    <x-guest-layout>
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="breakpoint-phone overflow-hidden text-ma-white grid grid-cols-3 gap-6">
-                @foreach ($videos as $video)
-                <div class="single-video">
-                    <div class="single-video-inner p-4">
-                        <a href="{{ route('video', ['id'=>$video->id]) }}">
-                            <div class="thumbnail-controller">
-                                <img src="https://img.youtube.com/vi/{{ $video->video_id }}/0.jpg" alt="">
-                            </div>
-                            <h2 class="video-title text-white font-bold pt-4">{{ __($video->title) }}</h2>
-                            <p class="mb-3 text-ma-white text-xs inline-block ml-2 mt-2">
-                                {{ __(App\Models\User::where(['id' => $video->user_id])->pluck('name')->first()) }}
-                            </p>
-                            @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                                <button
-                                    class="text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition inline-block float-left">
-                                    <img class="h-8 w-8 rounded-full object-cover inline-block"
-                                    src="{{ App\Models\User::where(['id' => $video->user_id])->pluck('profile_photo_path')->first() }}"
-                                    alt="{{ App\Models\User::where(['id' => $video->user_id])->pluck('name')->first() }}"/>
-                                </button>
-                            @endif
-                        </a>
+    @auth
+    @else
+        <x-guest-layout>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="breakpoint-phone overflow-hidden text-ma-white grid grid-cols-3 gap-6">
+                    @foreach ($videos as $video)
+                    <div class="single-video">
+                        <div class="single-video-inner p-4">
+                            <a href="{{ route('video', ['id'=>$video->id]) }}">
+                                <div class="thumbnail-controller">
+                                    <img src="https://img.youtube.com/vi/{{ $video->video_id }}/0.jpg" alt="">
+                                </div>
+                                <h2 class="video-title text-white font-bold pt-4">{{ __($video->title) }}</h2>
+                                <p class="mb-3 text-ma-white text-xs inline-block ml-2 mt-2">
+                                    {{ __(App\Models\User::where(['id' => $video->user_id])->pluck('name')->first()) }}
+                                </p>
+                                @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                                    <button
+                                        class="text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition inline-block float-left">
+                                        <img class="h-8 w-8 rounded-full object-cover inline-block"
+                                        src="{{ App\Models\User::where(['id' => $video->user_id])->pluck('profile_photo_path')->first() }}"
+                                        alt="{{ App\Models\User::where(['id' => $video->user_id])->pluck('name')->first() }}"/>
+                                    </button>
+                                @endif
+                            </a>
+                        </div>
                     </div>
+                    @endforeach
                 </div>
-                @endforeach
             </div>
-        </div>
-    </x-guest-layout>
+        </x-guest-layout>
+    @endauth
 </body>
 </html>
