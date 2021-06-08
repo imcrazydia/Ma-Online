8ec1c393819050637f56557b05cc4c3b3b1d18b4
Dani Rumping <43114717+PrinceCosmic@users.noreply.github.com>
2021-06-08 15:45:00 +0200

    Mobile link click nav fixed

--- ma_online/resources/views/vendor/jetstream/components/responsive-nav-link.blade.php
+++ ma_online/resources/views/vendor/jetstream/components/responsive-nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'block pl-3 pr-4 py-2 border-l-4 text-base font-medium text-white focus:outline-none focus:text-white focus:bg-indigo-100 focus:border-indigo-700 transition'
-            : 'block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-white hover:text-gray-800 hover:border-gray-300 focus:outline-none focus:text-white focus:bg-gray-50 focus:border-gray-300 transition';
+            ? 'block pl-3 pr-4 py-2 border-l-4 text-base font-medium text-white focus:outline-none focus:text-white focus:bg-magenta-100 focus:border-magenta-100 transition'
+            : 'block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-white hover:text-white hover:border-magenta-100 focus:outline-none focus:text-white focus:bg-magenta-100 focus:border-magenta-100 transition';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
